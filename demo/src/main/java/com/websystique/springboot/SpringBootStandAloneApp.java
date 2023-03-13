72b022cdaef433f5dfa86dd1e01a5d3116c166b9
nashida2210 <96041078+nashida2210@users.noreply.github.com>
2023-03-13 15:09:35 +0530

    Update SpringBootStandAloneApp.java

--- demo/src/main/java/com/websystique/springboot/SpringBootStandAloneApp.java
+++ demo/src/main/java/com/websystique/springboot/SpringBootStandAloneApp.java
@@ -7,20 +7,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.*;
 
 @EnableAutoConfiguration
-@Configuration
-public class RequestLoggingFilterConfig {
-
-    @Bean
-    public CommonsRequestLoggingFilter logFilter() {
-        CommonsRequestLoggingFilter filter
-          = new CommonsRequestLoggingFilter();
-        filter.setIncludeQueryString(true);
-        filter.setIncludePayload(true);
-        filter.setMaxPayloadLength(10000);
-        filter.setIncludeHeaders(false);
-        filter.setAfterMessagePrefix("REQUEST DATA: ");
-        return filter;
-    }
+@Bean
+public CommonsRequestLoggingFilter requestLoggingFilter() {
+    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+    loggingFilter.setIncludeClientInfo(true);
+    loggingFilter.setIncludeQueryString(true);
+    loggingFilter.setIncludePayload(true);
+    loggingFilter.setMaxPayloadLength(64000);
+    return loggingFilter;
 }
 @ComponentScans(
         {
